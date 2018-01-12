6b83c4524535e963e94e06166449c330a3d57e31
Oliver Moser <o@omoser.com>
2018-01-12 11:06:53 +0100

    Changing SRV name

--- dns_sd.tf
+++ dns_sd.tf
@@ -2,7 +2,7 @@
 resource "exoscale_domain_record" "prometheus_dns_sd_records" {
 	count = "${var.nodes_count}"
 	domain = "prometheus.exokube.eu"
-	name = "_prometheus._tcp.prometheus-sd"
+	name = "prometheus-sd"
 	record_type = "SRV"
 	content = "1 8080 prom-node-${count.index}.prometheus.exokube.eu."
 	ttl = 30

1ea5c3c687222ce8b15074a4b5643e3ae2756d91
Oliver Moser <o@omoser.com>
2018-01-14 16:04:43 +0100

    Externalizing domain

--- dns.tf
+++ dns.tf
@@ -1,9 +1,9 @@
 resource "exoscale_domain" "prometheus" {
-	name = "prometheus.exokube.eu"
+	name = "${var.domain}"
 }
 
 resource "exoscale_domain_record" "prometheus_master_record" {
-	domain = "prometheus.exokube.eu"
+	domain = "${var.domain}"
 	name = "${exoscale_compute.master.name}"
 	record_type = "A"
 	content = "${exoscale_compute.master.ip_address}"
@@ -12,7 +12,7 @@ resource "exoscale_domain_record" "prometheus_master_record" {
 
 resource "exoscale_domain_record" "prometheus_node_records" {
 	count = "${var.nodes_count}"
-	domain = "prometheus.exokube.eu"
+	domain = "${var.domain}"
 	name = "${element(exoscale_compute.nodes.*.name, count.index)}"
 	record_type = "A"
 	content = "${element(exoscale_compute.nodes.*.ip_address, count.index)}"
--- dns_sd.tf
+++ dns_sd.tf
@@ -1,9 +1,9 @@
 
 resource "exoscale_domain_record" "prometheus_dns_sd_records" {
 	count = "${var.nodes_count}"
-	domain = "prometheus.exokube.eu"
+	domain = "${var.domain}"
 	name = "prometheus-sd"
 	record_type = "SRV"
-	content = "1 8080 prom-node-${count.index}.prometheus.exokube.eu."
+	content = "1 8080 prom-node-${count.index}.${var.domain}."
 	ttl = 30
 }
